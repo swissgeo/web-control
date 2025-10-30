f9bb116fe5c9c5e9745c14967beb3974994d7646
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-10-30 13:57:36 +0100

    GPS-329 Enable Type Checking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
 
+  typescript: {
+    typeCheck: true,
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
