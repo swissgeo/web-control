8251bea2a9d14b75e1b140db2f100f83a20cafb0
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-10-30 15:14:46 +0100

    GPS-329 Add SVG support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     "@nuxt/test-utils",
     "@nuxt/ui",
     "@nuxtjs/i18n",
+    "nuxt-svgo",
   ],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
