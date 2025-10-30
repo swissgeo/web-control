c48cd807eb2a1b3697f3b7b9c55ee6d9b94f4fed
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-10-30 14:08:29 +0100

    GPS-329 Add Tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   ssr: false,
 
@@ -8,13 +10,14 @@ export default defineNuxtConfig({
     typeCheck: true,
   },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ],
+  css: ["./app/assets/css/main.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ["@nuxt/eslint", "@nuxt/image", "@nuxt/test-utils", "@nuxt/ui"],
   ui: {
-    fonts: false
-  }
-})
+    fonts: false,
+  },
+});
