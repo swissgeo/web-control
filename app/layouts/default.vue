abe79a6d3440e3f7cff7fdd045fee2ec1176bef5
Stefan Heinemann <stefan.heinemann@swisstopo.ch>
2025-10-31 11:56:34 +0100

    GPS-329 Install tests with playwright

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,7 +6,7 @@
       <template #title>
         <div class="flex items-center gap-8">
           <Logo />
-          <h1>Swissgeo Control</h1>
+          <h1 data-testid="global_title">Swissgeo Control</h1>
         </div>
       </template>
       <template #right>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,7 @@ export default defineNuxtConfig({
     "@nuxt/ui",
     "@nuxtjs/i18n",
     "nuxt-svgo",
+    "@nuxt/test-utils/module",
   ],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
