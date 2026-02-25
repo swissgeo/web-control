8563c18775a2e320cb6e6327263006edd8168730
Brice Schaffner <brice.schaffner@swisstopo.ch>
2026-02-25 15:26:58 +0100

    GPS-518: Improve the login page

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -30,15 +30,21 @@ function goToLogin() {
         root: 'p-2',
       }"
     />
-    <UPageBody>
+    <UPageBody class="flex flex-col items-center">
       <UPageSection
         :ui="{ container: 'py-4!' }"
         :title="$t('Login')"
         description="Welcome to the business portal. Please login"
+        class="flex w-max flex-col items-center text-center"
       >
-        <UButton icon="i-lucide-external-link" @click="goToLogin"
-          >Login</UButton
-        >
+        <div>
+          <UButton
+            icon="i-lucide-external-link"
+            class="max-w-max p-3 ps-5 pe-5"
+            @click="goToLogin"
+            >Login</UButton
+          >
+        </div>
       </UPageSection>
     </UPageBody>
   </UPage>
